395067a0dbd6f0be346d2a1edb264a83ccc0cc92
Alejandro Soto <alesoto9113@gmail.com>
2021-07-05 17:53:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = (schema) => {
+  schema.query.cursorBatched = async function * ({ batchSize = 10 }) {
+    const cursor = await this.cursor({ batchSize })
+    let batch = []
+    let hasNext = false
+    do {
+      const item = await cursor.next()
+      hasNext = !!item
+      if (hasNext) batch.push(item)
+      if (batch.length === batchSize) {
+        yield batch
+        batch = []
+      }
+    } while (hasNext)
+    if (batch.length) yield batch
+  }
+}
